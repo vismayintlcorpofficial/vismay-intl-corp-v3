95080f72eb36585c59e4b3ad2acb984ad96d934d
Ian Humprey <ian.humprey09@gmail.com>
2026-09-15 09:48:59 +0800

    Update Navigation

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -1,3 +1,49 @@
+import next from "next";
+import Image from "next/image";
+import Link from "next/link";
+import OurBrands from "../homepage/OurBrands";
+
 export default function Header() {
-  return <nav>Navigation Bar</nav>;
+   return (
+      <nav className="w-full mb-2">
+         <div className="flex justify-end gap-27 bg-[#06529B] py-1 px-35">
+            <Link className="text-lg font-semibold text-white" href="OurBrands">
+               Our Brands
+            </Link>
+            <Link className="text-lg font-semibold text-white" href="ContactUs">
+               Contact Us
+            </Link>
+         </div>
+         <div className="flex col justify-between bg-white px-35 py-1 items-center">
+            <Image
+               src="vismayLogo.svg"
+               alt="Vismay Logo"
+               width={104}
+               height={122}
+            />
+            <div className="flex col gap-19 items-center">
+               <Link href="/">
+                  <Image
+                     src="homeIcon.svg"
+                     alt="Vismay Logo"
+                     width={46}
+                     height={42}
+                  />
+               </Link>
+               <Link
+                  href="/who-we-are"
+                  className="text-xl font-bold text-[#06529B]"
+               >
+                  WHO WE ARE
+               </Link>
+               <Link
+                  href="/careers"
+                  className="text-xl font-bold text-[#06529B]"
+               >
+                  CAREERS
+               </Link>
+            </div>
+         </div>
+      </nav>
+   );
 }
