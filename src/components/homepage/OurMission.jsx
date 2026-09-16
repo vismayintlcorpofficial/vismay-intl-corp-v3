f952fd8553ff5de15f2c06b80ff1492df65ee1b5
Ian Humprey <ian.humprey09@gmail.com>
2026-09-16 10:14:51 +0800

    Our Mission Update

--- src/components/homepage/OurMission.jsx
+++ src/components/homepage/OurMission.jsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+
 export default function OurMission() {
-   return <section>OurMission</section>;
+   const images = [
+      "/images/ourMission/mission-1.svg",
+      "/images/ourMission/mission-2.svg",
+      "/images/ourMission/mission-3.svg",
+      "/images/ourMission/mission-4.svg",
+   ];
+
+   return (
+      <section className="w-full pb-10 bg-white">
+         <div className="overflow-x-auto justify-center flex scrollbar-hide">
+            <div className="flex gap-6 w-max px-8">
+               {images.map((image, index) => (
+                  <div key={index}>
+                     <Image
+                        src={image}
+                        alt={`Mission ${index + 1}`}
+                        width={300}
+                        height={300}
+                     />
+                  </div>
+               ))}
+            </div>
+         </div>
+      </section>
+   );
 }
