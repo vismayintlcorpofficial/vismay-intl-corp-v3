f0d7019dfd98c1862880f2cd7179a130b9b968e8
Ian Humprey <ian.humprey09@gmail.com>
2026-09-24 11:24:06 +0800

    Created Jobs

---
+++ src/components/careers/Jobs.jsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+
+export default function Jobs() {
+   return (
+      <section className="relative text-white">
+         {/* Text */}
+         <div className="relative z-10 bg-[#06529B] h-250 w-1/2 rounded-tr-[150px] p-3 md:px-15 xl:px-30 justify-center flex flex-col">
+            <h2 className="md:text-6xl">Careers with purpose</h2>
+            <p className="text-xl">
+               <br />
+               At Vismay, what you do matters. Whether you’re just starting out
+               or ready for a new challenge, your work can shape the world – and
+               your own future – all at once.
+               <br />
+               <br />
+               Currently we have job opportunities below:
+               <br />
+            </p>
+            <ul className="list-disc pl-3 md:pl-5 xl:pl-10 text-lg">
+               <li>Product and Sales Training Officer</li>
+               <li>Property Leasing Officer (Airbnb)</li>
+               <li>Project and Property Officer</li>
+               <li>Warehouse and Logistic Manager </li>
+               <li>IT Manager</li>
+               <li>FP&A Manager</li>
+               <li>Admin Officer</li>
+               <li>Livestreamer </li>
+               <li>General Manager (Paragon)</li>
+               <li>Accounting Supervisor</li>
+               <li> Key Accounts_Manager</li>
+            </ul>
+            <div className="flex lg:mt-10 lg:m-5 gap-5">
+               <p className="lg:px-10 2xl:px-14 py-1.5 border rounded-4xl">
+                  Jobstreet
+               </p>
+               <p className="lg:px-10 2xl:px-14 py-1.5 border rounded-4xl">
+                  Jobstreet
+               </p>
+               <p className="lg:px-10 2xl:px-14 py-1.5 border rounded-4xl">
+                  Jobstreet
+               </p>
+            </div>
+         </div>
+
+         <Image
+            src="/images/careers/jobs.svg"
+            alt=""
+            width={100}
+            height={100}
+            className="z-1 absolute top-0 right-0 w-5/9 h-250 object-cover"
+         />
+      </section>
+   );
+}
